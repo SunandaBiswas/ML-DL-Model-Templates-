d6bdb97184575eb6fd5a939eb96e4c43efc19edb
Sunanda Biswas <81425092+SunandaBiswas@users.noreply.github.com>
2021-09-15 06:31:01 +0600

    Create Basic data loading and exploration.py

---
+++ Basic data loading and exploration.py
@@ -0,0 +1,12 @@
+#..........................................................#
+### Import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+##........................................................##
+df = pd.read_csv(r'dataset.csv')
+df.head()
+
+##........................................................##
